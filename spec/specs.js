41494e179056877d7eec974c8c2aed3d2e6c9159
Ben Pritchard and Luke Chinworth <ben.s.pritchard@gmail.com>
2015-02-18 10:02:02 -0800

    Add specs and functions for a bank account object to deposit and withdraw.

---
+++ spec/specs.js
@@ -0,0 +1,18 @@
+describe("BankAccount", function() {
+  describe("deposit", function() {
+    it("increases the bank account's balance by a given amount", function() {
+      var account = Object.create(BankAccount);
+      account.deposit(10);
+      expect(account.balance).to.equal(10);
+    });
+  });
+
+  describe("withdraw", function() {
+    it("decreases the balance by the amount passed to it", function() {
+      var account = Object.create(BankAccount);
+      account.deposit(20);
+      account.withdraw(10);
+      expect(account.balance).to.equal(10);
+    });
+  });
+});
