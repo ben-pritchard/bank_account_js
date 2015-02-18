339e815d73c7a281505df013e8d4ab8035a770f7
Ben Pritchard and Luke Chinworth <ben.s.pritchard@gmail.com>
2015-02-18 12:17:10 -0800

    Add list of account holders and display balance of new accounts.

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,25 @@ var BankAccount = {
   }
 }
 
-$('#tabs a').click(function (e) {
-  e.preventDefault();
-  $(this).tab('show');
+$(document).ready(function() {
+  $("form#add-account").submit(function(event) {
+    event.preventDefault();
+    var name = $("input#name").val();
+    var initialDeposit = parseInt($("input#initial-deposit").val());
+
+    var newAccount = Object.create(BankAccount);
+    newAccount.name = name;
+    newAccount.deposit(initialDeposit);
+
+    $(".name-on-account").text(newAccount.name);
+    $(".balance").text(newAccount.balance);
+
+    $(".account-list").append("<li class='contact'>" + newAccount.name + "</li>")
+
+    $("input#name").val("");
+    $("input#initial-deposit").val("");
+
+    
+  });
+
 });
